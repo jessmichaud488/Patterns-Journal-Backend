53ceb690ea5ea1cfe77e8df6cf4b875a0be19afb
J M <jessmichaud488@gmail.com>
2019-10-23 20:06:32 -0400

    fixed index

--- src/auth/index.js
+++ src/auth/index.js
@@ -4,9 +4,9 @@ const {localStrategy, jwtStrategy} = require('./strategies');
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + 'index.html'))
 })
 module.exports = {router, localStrategy, jwtStrategy};
