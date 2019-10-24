115b29e1f6519a01a6d33b55619127e491520075
J M <jessmichaud488@gmail.com>
2019-10-23 20:02:34 -0400

    Deploy changes

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,4 +2,11 @@
 const {router} = require('./router');
 const {localStrategy, jwtStrategy} = require('./strategies');
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 module.exports = {router, localStrategy, jwtStrategy};
