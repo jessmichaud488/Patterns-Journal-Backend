13edca75ceaf006671ca1cfe974960e06883c652
Jessica <jessmichaud488@gmail.com>
2019-10-24 17:56:10 -0400

    Fixed Heroku deployment issues

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,11 +2,4 @@
 const {router} = require('./router');
 const {localStrategy, jwtStrategy} = require('./strategies');
 
-const path = require('path')
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'index.html'))
-})
 module.exports = {router, localStrategy, jwtStrategy};
